54e63afd6e0bfa4cb562bcd96464b36d697135b8
Tom Galvin <tom.galvin@landregistry.gsi.gov.uk>
2016-01-15 11:38:41 +0000

    Remove logging

--- application/__init__.py
+++ application/__init__.py
@@ -5,20 +5,3 @@ app = Flask(__name__)
 from application import views
 
 app.config.from_object(os.environ.get('SETTINGS'))
-
-# Set up logging if application has not been started in debug mode
-if not app.debug:
-    import logging
-    import sys
-
-    # Create log format
-    formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s', 
-                                  '%Y-%m-%d %H:%M:%S')
-
-    # Create console handler
-    console = logging.StreamHandler(sys.stdout)
-    console.setLevel(logging.INFO)
-    console.setFormatter(formatter)
-
-    # Enable log handlers
-    app.logger.addHandler(console)
