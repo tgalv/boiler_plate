5e8ad8bc9b38379caa4d0a851012c6e3e2da1a93
Tom Galvin <tom.galvin@landregistry.gsi.gov.uk>
2016-01-15 13:20:33 +0000

    Fix unittest

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,12 +2,12 @@ import unittest
 
 from application import app
 
-class TestBoilerPlate(unittest.TestCase):
+class TestHelloWorld(unittest.TestCase):
 
     def setUp(self):
         self.app = app.test_client()
 
     def test_service(self):
-        response = self.app.get('/')
+        response = self.app.get('/helloworld/')
         self.assertEquals("Hello World!", response.data)
         self.assertEquals(200, response.status_code)
