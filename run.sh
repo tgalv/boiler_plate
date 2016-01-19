9b97dacbfe80ee94bdea3400748090296266bc5c
Tom Galvin <tom.galvin@landregistry.gsi.gov.uk>
2016-01-19 09:03:26 +0000

    Entry Point WIP

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 export SETTINGS=config.DevelopmentConfig
 
 
-python run.py
+python boiler_plate/run.py
--- setup.py
+++ setup.py
@@ -5,10 +5,12 @@ from boiler_plate import __version__
 setup(
     name="boiler_plate",
     version = __version__,
-    author="Fred Bloggs",
-    author_email="fred.bloggs@landregistry.gsi.gov.uk",
+    author="Fix Me",
+    author_email="fix.me@landregistry.gsi.gov.uk",
     packages=find_packages(),
     install_requires=["flask"],
     tests_require=["flask"],
     test_suite="boiler_plate.test_suite.suite",
+    entry_points={'console_scripts':
+                  ['boiler_plate = boiler_plate.run:main']}
 )
