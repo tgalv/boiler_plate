94f2d5d9c1b6c2a927242c512d690bc02c50adde
Tom Galvin <tom.galvin@landregistry.gsi.gov.uk>
2016-01-28 10:32:08 +0000

    Add options to server init

--- boiler_plate/main.py
+++ boiler_plate/main.py
@@ -2,12 +2,36 @@
 Top level script for boiler_plate.
 """
 
+import os
+from optparse import OptionParser
 
-from boiler_plate import app
+import boiler_plate
 
 
 def main():
-    app.run(debug=True, host="0.0.0.0")
+    """
+    Run the boiler_plate server.
+    """
+    parser = OptionParser(usage="usage: %prog [options]",
+                          version="%prog {0}".format(boiler_plate.__version__))
+    _port = os.environ.get('PORT')
+    if _port is None:
+        _port = 5000
+    parser.add_option("-d", "--debug",
+                      action="store_true",
+                      dest="debug",
+                      default=False,
+                      help="Run Flask in debug")
+    parser.add_option("-p", "--port",
+                      dest="port",
+                      default=_port,
+                      help="Set the port")
+    parser.add_option("--host",
+                      dest="host",
+                      default="0.0.0.0",
+                      help="The host IP.")
+    (options, args) = parser.parse_args()
+    boiler_plate.app.run(debug=options.debug, host=options.host, port=int(options.port))
 
 
 if __name__ == "__main__":
